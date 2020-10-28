825f376183e29cd7be082bdeada7ed4942807ed2
EpicDoomSir <eitands@gmail.com>
2020-10-28 19:52:30 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Artist.destroy_all
+Genre.destroy_all
+Song.destroy_all
+
+artist1 = Artist.create(name: Faker::Artist.name, bio: Faker::Lorem.paragraph)
+artist2 = Artist.create(name: Faker::Artist.name, bio: Faker::Lorem.paragraph)
+artist3 = Artist.create(name: Faker::Artist.name, bio: Faker::Lorem.paragraph)
+
+genre1 = Genre.create(name: Faker::Cosmere.metal)
+genre2 = Genre.create(name: Faker::Cosmere.metal)
+genre3 = Genre.create(name: Faker::Cosmere.metal)
+
+
+
+song1 = Song.create(name: Faker::Cosmere.surge, artist_id: artist1.id, genre_id: genre1.id)
+song1 = Song.create(name: Faker::Cosmere.surge, artist_id: artist2.id, genre_id: genre2.id)
+song1 = Song.create(name: Faker::Cosmere.surge, artist_id: artist3.id, genre_id: genre3.id)
